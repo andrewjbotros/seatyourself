c6599ec35665c581c65f520460ff4e5cdc7d8ebb
andrewjbotros <andrewjbotros@gmail.com>
2014-02-10 16:41:55 -0500

    updates to user reservation view

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,5 +1,5 @@
 class Guest < ActiveRecord::Base
 	has_secure_password
 	has_many :reservations
-	has_many :restuarants, :through => :reservations
+	has_many :restaurants, :through => :reservations
 end
